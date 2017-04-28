99a7f7dd47b7b5edf214285ebf9cdafa8be329fe
Jeff Fontas <fontas.j+github@gmail.com>
2017-04-27 20:07:48 -0700

    Added Asset model

--- asset_store/api/models.py
+++ asset_store/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Assets(models.Model):
+    name = models.CharField(max_length=64, db_index=True, null=False)
+    asset_type = models.CharField(max_length=20, null=False)
+    asset_class = models.CharField(max_length=20, null=False)
--- asset_store/api/tests.py
+++ asset_store/api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from api.models import Assets
 
 # Create your tests here.
+class DataModelTest(TestCase):
+    def test_can_create_asset(self):
+        """Tests that an asset can be created"""
+        asset = Assets.objects.create(name="Flexidy", asset_type="satellite", asset_class="dove")
+        saved_assets = Assets.objects.all()
+        self.assertEqual(saved_assets[0].name, asset.name)
+
